a0d67a9fa2f9821b83af5b919fa18eb00bd15d0a
Richard Price <mrspondooli@gmail.com>
2018-09-17 18:38:28 +0100

    inital commit stylise UI

--- MyCompanies/AppDelegate.swift
+++ MyCompanies/AppDelegate.swift
@@ -8,6 +8,13 @@
 
 import UIKit
 
+class CustomNavigationController: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +23,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        let companiesController = ViewController()
+        let navController = CustomNavigationController(rootViewController: companiesController)
+        window?.rootViewController = navController
+        
+
+        
+        
+        
         return true
     }
 
--- MyCompanies/ViewController.swift
+++ MyCompanies/ViewController.swift
@@ -12,14 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        navigationItem.title = "My Companies"
+        setupNavStyle()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddButton))
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc fileprivate func handleAddButton() {
+        print("testing add button")
+        
     }
-
-
+    
+    fileprivate func setupNavStyle() {
+        let lightRed = UIColor(red: 247/255, green: 66/225, blue: 82/255, alpha: 1)
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.barTintColor = lightRed
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+    }
 }
 
