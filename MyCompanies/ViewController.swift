b90e5be61471884efd0da42cdde77c347437aea3
Richard Price <mrspondooli@gmail.com>
2018-09-18 19:46:48 +0100

    added uitableivew, with dummy cell and custom colors

--- MyCompanies/ViewController.swift
+++ MyCompanies/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,20 +16,51 @@ class ViewController: UIViewController {
         view.backgroundColor = .white
         navigationItem.title = "My Companies"
         setupNavStyle()
-
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddButton))
+        tableView.backgroundColor = .darkBlue
+        tableView.separatorStyle = .none
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableView.tableFooterView = UIView()
+    }
+    
+    //MARK:- UITableView Implementation
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.textLabel?.text = "My Test Company"
+        cell.backgroundColor = .tealColor
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        return cell
         
     }
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .lightBlue
+        return view
+    }
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    //MARK:-
+
     @objc fileprivate func handleAddButton() {
         print("testing add button")
-        
     }
     
     fileprivate func setupNavStyle() {
-        let lightRed = UIColor(red: 247/255, green: 66/225, blue: 82/255, alpha: 1)
-        navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.isTranslucent = true
-        navigationController?.navigationBar.barTintColor = lightRed
+        navigationController?.navigationBar.barTintColor = .lightRed
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
         navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
     }
 }
