510d3ecf95af877124ebc064b91c927a7ac8d0af
Richard Price <mrspondooli@gmail.com>
2018-09-22 14:25:40 +0100

    added custom delegation to send a company from create company controller

--- MyCompanies/CreateCompanyController.swift
+++ MyCompanies/CreateCompanyController.swift
@@ -8,24 +8,81 @@
 
 import UIKit
 
+protocol CreateCompanyControllerDelegate {
+    func didAddCompany(company: TestCompanies)
+}
+
 class CreateCompanyController: UIViewController {
+    
+    var delegate: CreateCompanyControllerDelegate?
+    
+    //MARK:- UI setup
+    let nameLable: UILabel = {
+        let lable = UILabel()
+        lable.text = "Name"
+        lable.textColor = .black
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        return lable
+    }()
+    
+    let lightBlueBackground: UIView = {
+        let view = UIView()
+        view.backgroundColor = .lightBlue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Company Name"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    //MARK:-
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
         view.backgroundColor = .darkBlue
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(handleSave))
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "cancel", style: .plain, target: self, action: #selector(handleCancel))
         navigationItem.title = "Create Company"
+        
+        
     }
     
     @objc fileprivate func handleSave() {
-        print("handle save testing")
+        dismiss(animated: true) {
+            guard let name =  self.nameTextField.text else {return}
+            let company = TestCompanies(name: name, founded: Date())
+            self.delegate?.didAddCompany(company: company)
+        }
     }
     
     @objc fileprivate func handleCancel() {
         
         dismiss(animated: true, completion: nil)
+        
+    }
+    
+    private func setupUI() {
+        view.addSubview(lightBlueBackground)
+        lightBlueBackground.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        lightBlueBackground.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        lightBlueBackground.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        lightBlueBackground.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        view.addSubview(nameLable)
+        nameLable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        nameLable.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16).isActive = true
+        nameLable.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        nameLable.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        view.addSubview(nameTextField)
+        nameTextField.leftAnchor.constraint(equalTo: nameLable.rightAnchor).isActive = true
+        nameTextField.topAnchor.constraint(equalTo: nameLable.topAnchor).isActive = true
+        nameTextField.bottomAnchor.constraint(equalTo: nameLable.bottomAnchor).isActive = true
+        nameTextField.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         
     }
 }
